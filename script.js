375c1f55d086f529cbe29b30ddce23cff98924d7
Pablo-Sousa02 <pablooficial22@hotmail.com>
2024-02-20 19:18:08 -0300

    front end finalizado

--- script.js
+++ script.js
@@ -18,3 +18,13 @@ var swiper = new Swiper('.swiper-container', {
       prevEl: '.swiper-button-prev',
     }
   });
+  function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+    }
+}
